7289513979522442e5150e0eaf615f6ae180a557
saburovser <saburovser@yandex.ru>
2022-11-22 22:49:38 +0300

    webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import { Bot } from 'grammy';
+import ngrok from 'ngrok';
+import express from 'express';
+import { Bot, webhookCallback } from 'grammy';
 
 import { MyContext, mySession } from './bot/session';
 import { getTitlesByName } from './api/anilist/getTitleByName';
@@ -10,7 +12,6 @@ const bot = new Bot<MyContext>(TOKEN ?? '');
 searchResultsMenu.register(searchedReleasesMenu);
 searchedReleasesMenu.register(searchedResolutionsMenu);
 bot.use(mySession, searchResultsMenu);
-
 
 bot.on('message', async ctx => {
   if (ctx.message.text === '/start') {
@@ -33,6 +34,19 @@ bot.catch((e) => {
   return e.ctx.reply(['```', JSON.stringify(e, undefined,' '), '```'].join('\n'), { parse_mode: 'MarkdownV2' });
 });
 
-bot.start({
-  onStart: () => console.log('Up and running!')
-}).catch(e => console.log(e));
+const server = express();
+server.use(express.json());
+server.post('/bot-webhook', webhookCallback(bot, 'express'), e => console.log(e));
+
+async function setup() {
+  if (process.env.NODE_ENV === 'development') {
+    const host = await ngrok.connect(Number(process.env.PORT));
+    console.log('ngrok connected on', host);
+    await bot.api.setWebhook(host + '/bot-webhook');
+  } else {
+    await bot.api.setWebhook(process.env.HOST ?? '');
+  }
+  server.listen(process.env.PORT);
+}
+
+setup().then(() => console.log('Up and running!'))
